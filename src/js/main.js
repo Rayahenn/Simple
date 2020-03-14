47edfc36b24e852db10a863e95e25593762aef35
rayahen <kubablaszczak1@gmail.com>
2020-03-14 22:08:30 +0100

    Add left menu and footer changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 const btn = document.querySelector('.kontakt__submit');
+const arrow = document.querySelector('.left-menu__arrow');
+const leftMenu = document.querySelector('nav.left-menu');
+
+
+arrow.addEventListener('click', () => {
+    arrow.classList.toggle('active');
+    if (arrow.classList.contains('active')) {
+        leftMenu.style.left = "0px";
+    }
+    else leftMenu.style.left = "-100px";
+    
+    
+})
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
